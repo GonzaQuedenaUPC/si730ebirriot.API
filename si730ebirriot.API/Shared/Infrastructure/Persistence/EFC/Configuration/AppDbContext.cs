44bd87063346bd3a5f9562aefd42340f3827df57
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-27 16:33:06 -0500

    feat(Shared): add shared resources.

---
+++ si730ebirriot.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+using si730ebirriot.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+namespace si730ebirriot.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+/// <summary>
+/// Application database context for the Learning Center Platform 
+/// </summary>
+/// <param name="options">
+/// The options for the database context
+/// </param>
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+   /// <summary>
+   /// On configuring the database context 
+   /// </summary>
+   /// <remarks>
+   /// This method is used to configure the database context.
+   /// It also adds the created and updated date interceptor to the database context.
+   /// </remarks>
+   /// <param name="builder">
+   /// The options builder for the database context
+   /// </param>
+   protected override void OnConfiguring(DbContextOptionsBuilder builder)
+   {
+      builder.AddCreatedUpdatedInterceptor();
+      base.OnConfiguring(builder);
+   }
+
+   /// <summary>
+   /// On creating the database model 
+   /// </summary>
+   /// <remarks>
+   /// This method is used to create the database model for the application.
+   /// </remarks>
+   /// <param name="builder">
+   /// The model builder for the database context
+   /// </param>
+   protected override void OnModelCreating(ModelBuilder builder)
+   {
+      base.OnModelCreating(builder);
+      
+      // Bounded Contexts
+      
+      // Apply snake case naming convention
+      builder.UseSnakeCaseNamingConvention();
+   }
+}
---
+++ si730ebirriot.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using si730ebirriot.API.Shared.Domain.Repositories;
+using si730ebirriot.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace si730ebirriot.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+/// <summary>
+/// Unit of work for the application.
+/// </summary>
+/// <remarks>
+/// This class is used to save changes to the database context.
+/// It implements the IUnitOfWork interface.
+/// </remarks>
+/// <param name="context">
+/// The database context for the application
+/// </param>
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    // inheritedDoc
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
