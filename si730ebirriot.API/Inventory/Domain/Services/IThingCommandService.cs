dce4cd6ae5c1edc44c6a0f89dce72ac79a0e5733
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-27 23:53:24 -0500

    feat(inventory): add command and query service contract.

---
+++ si730ebirriot.API/Inventory/Domain/Services/IThingCommandService.cs
@@ -0,0 +1,21 @@
+﻿using si730ebirriot.API.Inventory.Domain.Model.Aggregates;
+using si730ebirriot.API.Inventory.Domain.Model.Commands;
+
+namespace si730ebirriot.API.Inventory.Domain.Services;
+
+/// <summary>
+/// Service to handle Thing commands.
+/// </summary>
+public interface IThingCommandService
+{
+    /// <summary>
+    /// Handles the creation of a new Thing.
+    /// </summary>
+    /// <param name="command">
+    /// The command to create a new Thing.
+    /// </param>
+    /// <returns>
+    /// The created Thing.
+    /// </returns>
+    public Task<Thing> Handle(CreateThingCommand command);
+}
---
+++ si730ebirriot.API/Inventory/Domain/Services/IThingQueryService.cs
@@ -0,0 +1,21 @@
+﻿using si730ebirriot.API.Inventory.Domain.Model.Aggregates;
+using si730ebirriot.API.Inventory.Domain.Model.Queries;
+
+namespace si730ebirriot.API.Inventory.Domain.Services;
+
+/// <summary>
+/// Service to query Things.
+/// </summary>
+public interface IThingQueryService
+{
+    /// <summary>
+    /// Get a Thing by its ID.
+    /// </summary>
+    /// <param name="query">
+    /// The query to get a Thing by its ID.
+    /// </param>
+    /// <returns>
+    /// The Thing with the given ID.
+    /// </returns>
+    Task<Thing> Handle(GetThingByIdQuery query);
+}
