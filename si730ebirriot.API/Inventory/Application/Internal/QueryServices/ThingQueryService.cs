e89742195b2a1d8777302060b4aca12dee749a62
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-28 00:24:58 -0500

    feat(inventory): add thing query service implementation.

---
+++ si730ebirriot.API/Inventory/Application/Internal/QueryServices/ThingQueryService.cs
@@ -0,0 +1,29 @@
+﻿using si730ebirriot.API.Inventory.Domain.Model.Aggregates;
+using si730ebirriot.API.Inventory.Domain.Model.Queries;
+using si730ebirriot.API.Inventory.Domain.Repositories;
+using si730ebirriot.API.Inventory.Domain.Services;
+
+namespace si730ebirriot.API.Inventory.Application.Internal.QueryServices;
+
+/// <summary>
+/// Service to query Things.
+/// </summary>
+/// <param name="thingRepository">
+/// The repository for Things.
+/// </param>
+public class ThingQueryService(IThingRepository thingRepository) : IThingQueryService
+{
+    /// <summary>
+    /// The repository for Things.
+    /// </summary>
+    /// <param name="query">
+    /// The query to get a Thing by its ID.
+    /// </param>
+    /// <returns>
+    /// The Thing with the given ID.
+    /// </returns>
+    public async Task<Thing?> Handle(GetThingByIdQuery query)
+    {
+        return await thingRepository.FindByIdAsync(query.Id);
+    }
+}
