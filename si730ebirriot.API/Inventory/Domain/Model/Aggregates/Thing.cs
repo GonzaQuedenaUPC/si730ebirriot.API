6c2c4e2b971372a354d9391f52f3f5ba7a657863
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-27 23:42:42 -0500

    feat(inventory): add thing aggregate.

---
+++ si730ebirriot.API/Inventory/Domain/Model/Aggregates/Thing.cs
@@ -0,0 +1,65 @@
+﻿using si730ebirriot.API.Inventory.Domain.Model.Commands;
+using si730ebirriot.API.Inventory.Domain.Model.ValueObjects;
+
+namespace si730ebirriot.API.Inventory.Domain.Model.Aggregates;
+
+/// <summary>
+/// Aggregate root for Thing entity
+/// </summary>
+public partial class Thing
+{
+    /// <summary>
+    /// Unique identifier for the Thing
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Serial number of the Thing
+    /// </summary>
+    public SerialNumber SerialNumber { get; set; }
+    
+    /// <summary>
+    /// Model of the Thing
+    /// </summary>
+    public string Model { get; set; }
+    
+    /// <summary>
+    /// Operation mode of the Thing
+    /// </summary>
+    public EOperationMode OperationMode { get; set; }
+    
+    /// <summary>
+    /// Maximum temperature threshold of the Thing
+    /// </summary>
+    public decimal MaximumTemperatureThreshold { get; set; }
+    
+    /// <summary>
+    /// Minimum temperature threshold of the Thing 
+    /// </summary>
+    public decimal MinimumTemperatureThreshold { get; set; }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public Thing()
+    {
+        Model = "";
+        MaximumTemperatureThreshold = 0;
+        MinimumTemperatureThreshold = 0;
+        SerialNumber = new SerialNumber();
+        OperationMode = EOperationMode.SCHEDULE_DRIVEN;
+    }
+
+    /// <summary>
+    /// Constructor with parameters
+    /// </summary>
+    /// <param name="command">
+    /// Command to create a Thing
+    /// </param>
+    public Thing(CreateThingCommand command) : this()
+    {
+        this.Model = command.Model;
+        this.MaximumTemperatureThreshold = command.MaximumTemperatureThreshold;
+        this.MinimumTemperatureThreshold = command.MinimumTemperatureThreshold;
+    }
+}
---
+++ si730ebirriot.API/Inventory/Domain/Model/Aggregates/ThingAudit.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace si730ebirriot.API.Inventory.Domain.Model.Aggregates;
+
+/// <summary>
+/// Represents a Thing with audit fields.
+/// </summary>
+public partial class Thing
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
